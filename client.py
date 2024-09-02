823fbb81f7d2cd6e5bc76837ea74e0badab76bd1
Boris <kabv@mail.ru>
2024-09-02 23:23:00 +0600

    Initial commit

--- client.py
+++ client.py
@@ -1,32 +1,29 @@
 import requests
 
 
-# response = requests.post(
-#     'http://127.0.0.1:5000/advertisements/',
-#     json={
-#         'title': 'title1',
-#         'description': 'description1',
-#         'user': '1'
-#     }
-# )
+response = requests.post(
+    'http://127.0.0.1:5000/advertisements/',
+    json={
+        'title': 'title1',
+        'description': 'description1',
+        'user': '1'
+    }
+)
 
-# response = requests.post(
-#     'http://127.0.0.1:5000/advertisements/1',
-#     json={
-#         'title': 'title1111'
-#     }
-# )
+response1 = requests.patch(
+    'http://127.0.0.1:5000/advertisements/1',
+    json={
+        'title': 'title1111'
+    }
+)
 
-response = requests.get(
+response2 = requests.get(
     'http://127.0.0.1:5000/advertisements/1',
 )
 
-# response = requests.delete(
-#     'http://127.0.0.1:5000/advertisements/',
-#     json={
-#         'id': '1'
-#     }
-# )
+response3 = requests.delete(
+    'http://127.0.0.1:5000/advertisements/1',
+)
 
 print(response.status_code)
 print(response.json())
