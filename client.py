6f7592eff63ee75c5b86d067832e7506d0b4a0cd
Boris <kabv@mail.ru>
2024-09-18 23:54:20 +0600

    Initial commit

--- client.py
+++ client.py
@@ -1,29 +1,40 @@
 import requests
+import asyncio
+import aiohttp
 
 
-response = requests.post(
-    'http://127.0.0.1:5000/advertisements/',
-    json={
-        'title': 'title1',
-        'description': 'description1',
-        'user': '1'
-    }
-)
+async def main():
+    async with aiohttp.ClientSession() as session:
+        response = await session.post("http://127.0.0.1:8080/hello/world")
+        print(response.status)
+        print(await response.text())
 
-response1 = requests.patch(
-    'http://127.0.0.1:5000/advertisements/1',
-    json={
-        'title': 'title1111'
-    }
-)
 
-response2 = requests.get(
-    'http://127.0.0.1:5000/advertisements/1',
-)
 
-response3 = requests.delete(
-    'http://127.0.0.1:5000/advertisements/1',
-)
+asyncio.run(main())
+# response = requests.post(
+#     'http://127.0.0.1:5000/advertisements/',
+#     json={
+#         'title': 'title1',
+#         'description': 'description1',
+#         'user': '1'
+#     }
+# )
 
-print(response.status_code)
-print(response.json())
+# response1 = requests.patch(
+#     'http://127.0.0.1:5000/advertisements/1',
+#     json={
+#         'title': 'title1111'
+#     }
+# )
+#
+# response2 = requests.get(
+#     'http://127.0.0.1:5000/advertisements/1',
+# )
+#
+# response3 = requests.delete(
+#     'http://127.0.0.1:5000/advertisements/1',
+# )
+#
+# print(response.status_code)
+# print(response.json())
--- server.py
+++ server.py
@@ -2,57 +2,68 @@ import flask
 from flask import request, jsonify
 from flask.views import MethodView
 from models import Advertisements, Session, User
+from aiohttp import web
 
-app = flask.Flask('app')
-
-
-def hello():
-    return {'hello': 'world'}
+app = web.Application()
 
 
-class UserView(MethodView):
-    def post(self):
-        pass
+async def hello_world():
+    response = web.json_response({"hello": "world"})
+    return response
 
 
-class AdvertisementsView(MethodView):
-    def post(self):
-        json_data = request.json
-        advertisement = Advertisements(**json_data)
-        request.session.add(advertisement)
-        request.session.commit()
-        return {'id': advertisement.id}
-
-    def patch(self, id):
-        json_data = request.json
-        advertisement = request.session.get(Advertisements, id=id)
-        for k, v in json_data:
-            advertisement[k] = v
-        request.session.commit()
-        return jsonify(advertisement.json)
-
-    def delete(self, id):
-        request.session.delete(Advertisements, id=id)
-        request.session.commit()
-        return {'id': 'deleted'}
-
-    def get(self, id: int):
-        advertisement = request.session.get(Advertisements, id=id)
-        return jsonify(advertisement.json)
+app.add_routes([
+    web.post("/hello/world", hello_world)
+])
 
 
-@app.before_request
-def before_request():
-    session = Session()
-    request.session = session
 
 
-@app.after_request
-def after_request(http_response: flask.Response):
-    request.session.close()
-    return http_response
+web.run_app(app, host='127.0.0.1', port=8080)
 
-
-app.add_url_rule('/advertisements/', view_func=AdvertisementsView.as_view('advertisements'), methods=['POST', 'GET',
-                                                                                                'DELETE', 'UPDATE'])
-app.run()
+# class UserView(MethodView):
+#     def post(self):
+#         pass
+#
+#
+# class AdvertisementsView(MethodView):
+#     def post(self):
+#         json_data = request.json
+#         advertisement = Advertisements(**json_data)
+#         request.session.add(advertisement)
+#         request.session.commit()
+#         return {'id': advertisement.id}
+#
+#     def patch(self, id):
+#         json_data = request.json
+#         advertisement = request.session.get(Advertisements, id=id)
+#         for k, v in json_data:
+#             advertisement[k] = v
+#         request.session.commit()
+#         return jsonify(advertisement.json)
+#
+#     def delete(self, id):
+#         request.session.delete(Advertisements, id=id)
+#         request.session.commit()
+#         return {'id': 'deleted'}
+#
+#     def get(self, id: int):
+#         advertisement = request.session.get(Advertisements, id=id)
+#         return jsonify(advertisement.json)
+#
+#
+# @app.before_request
+# def before_request():
+#     session = Session()
+#     request.session = session
+#
+#
+# @app.after_request
+# def after_request(http_response: flask.Response):
+#     request.session.close()
+#     return http_response
+#
+#
+# app.add_url_rule('/advertisements/', view_func=AdvertisementsView.as_view('advertisements'), methods=['POST', 'GET',
+#                                                                                                 'DELETE', 'UPDATE'])
+# app.run()
